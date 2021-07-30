d997ca96da1b0d21cf87691180cb3ec95e6ab5ed
Yannick Uhlmann <augustunderground@protonmail.com>
2021-07-30 18:22:46 +0200

    FreeBSD

--- .config/lf/preview.sh
+++ .config/lf/preview.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
+if [ -n "$(command -v chafa)" ]; then
+    IMVIEWER=chafa
+elif [ -n "$(command -v pixterm)" ]; then
+    IMVIEWER=pixterm
+elif [ -n "$(command -v termpix)" ]; then
+    IMVIEWER=termpix
+else
+    IMVIEWER=file
+fi
+ 
 case "$1" in
     *.tar*) tar tf "$1";;
     *.zip) unzip -l "$1";;
     *.rar) unrar l "$1";;
     *.7z) 7z l "$1";;
     *.pdf) pdftotext "$1" -;;
-    *.jpg|*.png|*.bmp|*.gif) pixterm "$1";;
+    *.jpg|*.png|*.bmp|*.gif) $IMVIEWER "$1";;
     *) bat "$1";;
 esac
