e36d5d018052e4a0547a13b6e77778ded7801d10
Yannick Uhlmann <augustunderground@protonmail.com>
2021-08-13 22:33:47 +0200

    jpeg

--- .config/lf/preview.sh
+++ .config/lf/preview.sh
@@ -16,6 +16,6 @@ case "$1" in
     *.rar) unrar l "$1";;
     *.7z) 7z l "$1";;
     *.pdf) pdftotext "$1" -;;
-    *.jpg|*.png|*.bmp|*.gif) $IMVIEWER "$1";;
+    *.jpg|*.jpeg|*.png|*.bmp|*.gif) $IMVIEWER "$1";;
     *) bat "$1";;
 esac
