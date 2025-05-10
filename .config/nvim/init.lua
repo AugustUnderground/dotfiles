c1efa3916c33d143589f943d0d1f5ef99f1afb36
Yannick Uhlmann <augustunderground@protonmail.com>
2025-05-10 13:28:22 +0200

    clangd and perlpls

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -640,6 +640,8 @@ lsp["fortls"].setup({ capabilities = capabilities })
 lsp["julials"].setup({ capabilities = capabilities })
 lsp["agda_ls"].setup({ capabilities = capabilities })
 lsp["quick_lint_js"].setup({ capabilities = capabilities })
+lsp["clangd"].setup({ capabilities = capabilities })
+lsp["perlpls"].setup({ capabilities = capabilities })
 
 if jit.os == "OSX" then
   lsp["sourcekit"].setup({ capabilities = capabilities })
